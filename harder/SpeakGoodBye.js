afdcf6aff74e629bae739b4c1b0606e3d2fe6a8d
Unknown6699 <131878171+Unknown6699@users.noreply.github.com>
2023-06-01 15:11:33 +0300

    Delete harder directory

--- harder/SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-
-(function(window) {
-  const speakWord = "Good Bye"
-  const byeSpeaker = {}
-  byeSpeaker.speak = function(name) {
-    console.log(`%c ${speakWord} ${name}`, "font-size: 20px; font-weight: bold; color: Red;")
-  }
-  window.byeSpeaker = byeSpeaker
-})(window)
--- harder/SpeakHello.js
+++
@@ -1,9 +0,0 @@
-
-(function(window) {
-  const speakWord = "Hello";
-  const helloSpeaker = {}
-  helloSpeaker.speak = function(name) {
-    console.log(`%c ${speakWord} ${name}`, "font-size: 20px; font-weight: bold; color: Lime;")
-  }
-  window.helloSpeaker = helloSpeaker
-})(window)
